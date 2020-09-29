715fde4165cef5d7872c1143d064c2c9659fb6d2
BonusGned <jesus.moj@mail.com>
2020-09-29 16:07:30 +0300

    add middle rating

--- movies/views.py
+++ movies/views.py
@@ -3,12 +3,18 @@ from rest_framework.views  import APIView
 
 from .models import Movie
 from .serializers import MovieListSerializer, MovieDetailSerializer, ReviewCreateSerializer, CreateRatingSerializer
+from .service import get_client_ip
+from django.db import models
 
 
 class MovieListView(APIView):
     '''Вывод списка фильмов'''
     def get(self, request):
-        movies = Movie.objects.filter(draft=False)
+        movies = Movie.objects.filter(draft=False).annotate(
+            rating_user=models.Count('ratings', filter=models.Q(ratings__ip=get_client_ip(request)))
+        ).annotate(
+            middle_star=models.Sum(models.F('ratings__star')) / models.Count(models.F('ratings'))
+        )
         serializer = MovieListSerializer(movies, many=True)
         return Response(serializer.data)
 
@@ -41,7 +47,7 @@ class AddStarRatingView(APIView):
     def post(self, request):
         serializer = CreateRatingSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(ip=self.get_client_ip(request))
+            serializer.save(ip=get_client_ip(request))
             return Response(status=201)
         else:
             return Response(status=400)
