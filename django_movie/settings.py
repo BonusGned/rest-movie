e351f89f3b89becb5103c11498da4c663455e7cc
BonusGned <jesus.moj@mail.com>
2020-09-30 19:21:24 +0300

    add oauth2 vk

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -42,9 +42,19 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'ckeditor',
     'ckeditor_uploader',
-    'movies',
-    'django_filters',
     'djoser',
+
+    'oauth2_provider',
+    'social_django',
+    'rest_framework_social_oauth2',
+
+    # 'drf_yasg',
+
+
+    'django_filters',
+
+
+    'movies',
 ]
 
 MIDDLEWARE = [
@@ -209,10 +219,22 @@ CKEDITOR_CONFIGS = {
     }
 }
 
+SOCIAL_AUTH_VK_OAUTH2_KEY = '7610995'
+SOCIAL_AUTH_VK_OAUTH2_SECRET = 'IXYzvyqK5pJQKfEJiXM8'
+
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.vk.VKOAuth2',
+    'rest_framework_social_oauth2.backends.DjangoOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+        'rest_framework_social_oauth2.authentication.SocialAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
--- django_movie/urls.py
+++ django_movie/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+# from .yasg import urlpatterns as doc_urls
 
 from django.conf import settings
 
@@ -23,11 +24,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('auth/', include('rest_framework_social_oauth2.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls.jwt')),
     path('api/v1/', include('movies.urls')),
 ]
+
+# urlpatterns += doc_urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,  document_root=settings.MEDIA_ROOT)
